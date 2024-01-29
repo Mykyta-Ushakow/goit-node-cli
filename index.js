5d645d6aaebb309434e78e7841e4aab2bdd8a14a
Mykyta-Ushakow <nilitosushak@gmail.com>
2024-01-29 16:30:31 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { program } from "commander";
+import fs from "fs/promises";
+
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+	.option("-a, --action <type>", "choose action")
+	.option("-i, --id <type>", "user id")
+	.option("-n, --name <type>", "user name")
+	.option("-e, --email <type>", "user email")
+	.option("-p, --phone <type>", "user phone");
 
 program.parse();
 
@@ -12,26 +14,28 @@ const options = program.opts();
 
 // TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+	switch (action) {
+		case "list":
+			// ...
+			break;
 
-    case "get":
-      // ... id
-      break;
+		case "get":
+			// ... id
+			break;
 
-    case "add":
-      // ... name email phone
-      break;
+		case "add":
+			// ... name email phone
+			break;
 
-    case "remove":
-      // ... id
-      break;
+		case "remove":
+			// ... id
+			break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
+		default:
+			console.warn("\x1B[31m Unknown action type!");
+	}
 }
 
-invokeAction(options);
+// invokeAction(options);
+
+console.log("first");
